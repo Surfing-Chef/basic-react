3f8cb4416badc655b793c9e4ce8f28514bf540a8
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-07-30 13:10:09 -0700

    Reset to basic files

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Layout from "./components/Layout";
+class Layout extends React.Component {
+  render() {
+    return (
+      <h1>It's Working!</h1>
+    )
+  }
+}
 
 const app = document.getElementById('app');
+
 ReactDOM.render(<Layout/>, app);
