941b6c45717b75601c5be034ecf103fb6f38a02d
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-07-30 21:07:43 -0700

    Explored events and data changes

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -11,15 +11,14 @@ export default class Layout extends React.Component {
     };
   }
 
-  render() {
-    setTimeout(() => {
-      this.setState({title: "Welcome Wade!"});
-    }, 2000)
+  changeTitle(title) {
+    this.setState({title});
+  }
 
+  render() {
     return (
       <div>
-        <Header title={this.state.title}/>
-        <Header title={"Other Title"}/>
+        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
         <Footer/>
       </div>
     )
